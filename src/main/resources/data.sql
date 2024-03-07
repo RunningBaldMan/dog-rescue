e1f201344066b1f2b00e5c81e6387a5f9c7a8db1
RunningBadlMan <150305321+RunningBaldMan@users.noreply.github.com>
2024-03-07 14:10:21 -0500

    Week 15 dog rescue

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO breed (name) VALUES ('Bulldog');
+INSERT INTO breed (name) VALUES ('Labrador Retriever');
+INSERT INTO breed (name) VALUES ('German Shepard');
+INSERT INTO breed (name) VALUES ('Alaskan Malamute');
+INSERT INTO breed (name) VALUES ('Golden Retriever');
+INSERT INTO breed (name) VALUES ('Affenpincher');
+INSERT INTO breed (name) VALUES ('Airedale Terrier');
+INSERT INTO breed (name) VALUES ('French Bulldog');
+INSERT INTO breed (name) VALUES ('Irish Setter');
